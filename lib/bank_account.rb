6fe382b827b2df3f94370d2216a93383f3410cf9
Dana Simmons <dana@simmons-audio.com>
2019-01-31 15:38:39 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
+require "pry"
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance
+  attr_accessor :status
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
 
+  def deposit(deposit)
+    @balance = @balance + deposit
+  end
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+  def valid?
+    if @balance > 0 && @status == 'open'
+      true
+    else
+      false
+    end
+  end
+  def close_account
+    @status = 'closed'
+  end
 end
